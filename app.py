7c98d44fa781e72b8f34a3f1fa8b06b1488f0c8c
Arianvba24 <153762786+Arianvba24@users.noreply.github.com>
2024-11-13 00:51:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,10 @@ def get_driver():
 options = Options()
 options.add_argument("--disable-gpu")
 options.add_argument("--headless")
-
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-blink-features=AutomationControlled")  # Evita la detección como bot
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
 user = "ferragutarian@gmail.com"
 passwd = "03051997Leandro*"
